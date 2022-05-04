433874334c9f4e58d4d31cbe27de162048eae2b4
Terry-404 <gamecorner71@gmail.com>
2022-05-04 09:29:04 +0200

    Accept upgrade only in '/upload'

--- server.js
+++ server.js
@@ -11,6 +11,11 @@ wserver.on('connection',(ws)=>{
 
 //in case of upgrade (from HTTP -> WebSocket), emit the connection to the WS server.
 server.on('upgrade',(request,socket,head)=>{
+    //only accept upgrades in "/upload" path.
+    if(request.url !== '/upload'){
+        socket.end();
+    }
+
     wserver.handleUpgrade(request,socket,head, (ws)=>{
         wserver.emit('connection', ws, request)
     })
@@ -21,6 +26,10 @@ server.on('request',(req,res)=>{
     if(req.url === '/'){
         res.end(fs.readFileSync('./src/index.html'))
         return;
+    }
+
+    if(req.url === 'upload'){
+        res.end('You may need to upgrade protocol to WebSocket.')
     }
 })
 
