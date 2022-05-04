8266ae44e2deeffd606885cbc5bc9994b88db8f0
Terry-404 <gamecorner71@gmail.com>
2022-05-04 09:22:31 +0200

    Simple WS x HTTP server setup

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
-const WebSocket = require('ws');
+const ws = require('ws');
+const fs = require('fs');
+const http = require('http');
 
-const server = new WebSocket.Server({
-    port:3000
-}, ()=>{console.log('server is up and running.')})
+const server = http.createServer()
+const wserver = new ws.Server({noServer:true})
 
-
-server.on('connection', (socket, req)=>{
-    socket.send("ehm")
+wserver.on('connection',(ws)=>{
+    ws.send("test.")
 })
+
+//in case of upgrade (from HTTP -> WebSocket), emit the connection to the WS server.
+server.on('upgrade',(request,socket,head)=>{
+    wserver.handleUpgrade(request,socket,head, (ws)=>{
+        wserver.emit('connection', ws, request)
+    })
+})
+
+//in case of Request (HTTP) Act like a normal HTTP server.
+server.on('request',(req,res)=>{
+    if(req.url === '/'){
+        res.end(fs.readFileSync('./src/index.html'))
+        return;
+    }
+})
+
+server.listen(process.env.PORT || 3000)
